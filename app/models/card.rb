e8ab6cc9399bf8a048a4faa00ba54e83b30d02eb
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-04 20:04:24 +0300

    fix texts_must_be_diffirent validation & change set_review_date

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,19 +6,17 @@ class Card < ApplicationRecord
   validates :review_date, presence: true
   validate :texts_must_be_diffirent
 
-  def view
-    update(review_date: Time.now)
-  end
-
   private
 
   def texts_must_be_diffirent
-    if original_text.downcase == translated_text.downcase
+    if original_text.mb_chars.downcase == translated_text.mb_chars.downcase
       errors.add(:texts, 'must be different')
     end
   end
 
   def set_review_date
-    self.review_date = Time.now + 3.days
+    unless self.review_date
+      self.review_date = Time.now + 3.days
+    end
   end
 end

e4e1a6c80057b72ad06ea94f575d683ecb02de0a
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-04 14:07:09 +0300

    modify Card controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,6 @@
 class CardsController < ApplicationController
 
-  before_action :set_card, only: :show
+  before_action :set_card, only: [:show, :edit, :update, :destroy]
 
   def index
     @cards = Card.all
@@ -14,16 +14,33 @@ class CardsController < ApplicationController
     @card = Card.new
   end
 
+  def edit
+  end
+
   def create
     @card = Card.new(card_params)
 
     if @card.save
-      redirect_to cards_path
+      redirect_to cards_path, notice: 'Карточка добавлена'
     else
       render :new
     end
   end
 
+  def update
+    @card.update(card_params)
+
+    if @card.save
+      redirect_to cards_path, notice: 'Карточка обновлена'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to cards_path, notice: 'Карточка удалена'
+  end
 
   private
 
@@ -33,7 +50,6 @@ class CardsController < ApplicationController
 
   def card_params
     params.require(:card).permit(:original_text,
-                                 :translated_text,
-                                 :review_date)
+                                 :translated_text)
   end
 end

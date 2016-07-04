dfd3d6b2504284dd34c5aac0dae742a9160d305f
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-04 10:40:18 +0300

    Card model: add validations & set review date before validation

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,24 @@
 class Card < ApplicationRecord
+  before_validation :set_review_date
+
+  validates :original_text, presence: true
+  validates :translated_text, presence: true
+  validates :review_date, presence: true
+  validate :texts_must_be_diffirent
 
   def view
     update(review_date: Time.now)
   end
 
+  private
+
+  def texts_must_be_diffirent
+    if original_text.downcase == translated_text.downcase
+      errors.add(:texts_must_be_diffirent, 'оригинал и перевод должны отличаться')
+    end
+  end
+
+  def set_review_date
+    self.review_date = Time.now + 3.days
+  end
 end

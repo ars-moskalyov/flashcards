9eca49e625a1c4668b460180c10aa4cdcc9e3995
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-05 22:06:26 +0300

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
+require 'open-uri'
+require 'nokogiri'
+
 url = 'http://russian.languagedaily.com/wordsandphrases/russian-cognates'
 html = open(url)
 
-# загружаем станицу в nokogiri
 page = Nokogiri::HTML(html)
-# выдираем нужный фрагмент
 words = page.css('.jsn-article-content')
-# находим по тегу нужные элементы, убираем тэги, получаем массив
 words = words.css('li').map { |tag| tag.text }
 
 pair_words = words.map do |item|

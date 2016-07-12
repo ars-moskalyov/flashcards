e548607ca7a5ae5650f727d6313df59979d28566
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-12 21:19:27 +0300

    add user validations tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations tests' do
+    subject { create(:user) }
+    it { should validate_length_of(:password).is_at_least(5) }
+    it { should validate_confirmation_of :password }
     it { should validate_presence_of :email }
-    it { should validate_uniqueness_of :email }
+    it { should validate_uniqueness_of(:email).case_insensitive }
   end
   
   describe 'associations tests' do

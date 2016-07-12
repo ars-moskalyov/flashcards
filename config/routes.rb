ed0b1737ed6592de8b4ca9516f5e5786fccca831
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-12 20:12:08 +0300

    add user sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   post 'trainer/review'
   resources :cards
-  resources :users
+  resources :users, only: [:new, :create]
+  resources :user_sessions, only: [:create, :destroy]
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end

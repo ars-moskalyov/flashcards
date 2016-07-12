968cf34af4f120655e79f5963d57b53a0ce23b21
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-12 21:15:48 +0300

    add email validate to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
+  validates_uniqueness_of :email, case_sensitive: false
 end

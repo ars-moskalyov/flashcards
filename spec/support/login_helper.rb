607951af73d4629b3a2d7cdeb3ae67881a22ee01
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-13 19:37:53 +0300

    locale login button

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -3,5 +3,5 @@ def login(email, password)
   click_link I18n.t('views.layout.login')
   fill_in 'session[email]', with: email
   fill_in 'session[password]', with: password
-  click_button 'Save Session'
+  click_button I18n.t('views.session.sign_in')
 end

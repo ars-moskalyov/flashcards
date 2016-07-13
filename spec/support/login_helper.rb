8c070273d7abde02f6576d849da5962f02b205b5
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-13 19:25:45 +0300

    fix test

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -1,7 +1,7 @@
 def login(email, password)
   visit root_path
   click_link I18n.t('views.layout.login')
-  fill_in 'user_session[email]', with: email
-  fill_in 'user_session[password]', with: password
-  click_button 'Save User session'
+  fill_in 'session[email]', with: email
+  fill_in 'session[password]', with: password
+  click_button 'Save Session'
 end

6a678dae81537ff559277ee8dcc422039ec18a93
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-13 19:22:26 +0300

    edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,24 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  before_action :require_login
+  before_action :set_user
+
+  def edit
   end
 
-  def create
-    @user = User.new(user_params)
+  def update
+    @user.update(user_params)
     if @user.save
-      login(user_params[:email], user_params[:password])
-      redirect_to root_path, notice: I18n.t('controllers.user.created')
+      redirect_to root_path, notice: I18n.t('controllers.user.updated')
     else
-      render :new
+      render :edit
     end
   end
 
   private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)

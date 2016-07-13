8084878b86d76d1e81e4b1a844ba70a540dc90cc
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-13 19:57:37 +0300

    fix new card spec

--- spec/features/card/add_new_card_spec.rb
+++ spec/features/card/add_new_card_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 
-# feature 'add new card' do
-#   scenario 'all fields are filled' do
-#     visit new_card_path
-#     fill_in 'card[original_text]', with: 'original text'
-#     fill_in 'card[translated_text]', with: 'translated text'
-#     click_button I18n.t('views.card.save_card')
-#     expect(page).to have_content I18n.t('controllers.card.create')
-#     expect(page).to have_content 'original text translated text'
-#   end
+feature 'add new card' do
+  before(:each) do
+    @user = create(:user, email: 'qqq@www')
+    login('qqq@www', 'password')
+  end
+  scenario 'all fields are filled' do
+    visit new_card_path
+    fill_in 'card[original_text]', with: 'original text'
+    fill_in 'card[translated_text]', with: 'translated text'
+    click_button I18n.t('views.card.save_card')
+    expect(page).to have_content I18n.t('controllers.card.create')
+    expect(page).to have_content 'original text translated text'
+  end
 
-#     scenario 'the translated text field is not filled' do
-#     visit new_card_path
-#     fill_in 'card[original_text]', with: 'original text'
-#     click_button I18n.t('views.card.save_card')
-#     expect(page).to have_content I18n.t('views.card.new_card')
-#     expect(page).to have_content I18n.t('errors.messages.blank')
-#   end
-# end 
+    scenario 'the translated text field is not filled' do
+    visit new_card_path
+    fill_in 'card[original_text]', with: 'original text'
+    click_button I18n.t('views.card.save_card')
+    expect(page).to have_content I18n.t('views.card.new_card')
+    expect(page).to have_content I18n.t('errors.messages.blank')
+  end
+end 

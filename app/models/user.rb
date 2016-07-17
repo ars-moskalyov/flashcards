d339195e2cd7c2b83045896cb81b67ffdc54e2a4
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-17 23:44:39 +0300

    add default_deck to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   
   has_many :decks, dependent: :destroy
-
+  belongs_to :default_deck, class_name: 'Deck', foreign_key: "default_deck_id"
+  
   validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
@@ -11,7 +12,7 @@ class User < ApplicationRecord
 
   def review_card
     if default_deck_id
-      decks.find(default_deck_id).cards.review.first
+      default_deck.cards.review.first
     else
       decks.order("RANDOM()").first.cards.review.first
     end

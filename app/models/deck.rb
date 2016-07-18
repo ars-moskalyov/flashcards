b7e68785759c6d220a3de12a0ec56e6fb42ba50d
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-18 09:51:42 +0300

    remove default deck id after destroy

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -5,4 +5,14 @@ class Deck < ApplicationRecord
   validates :user, presence: true
   validates :title, presence: true
   validates :description, presence: true
+
+  after_destroy :remove_default
+
+  private
+
+  def remove_default
+    if user.default_deck_id == id
+      user.update(default_deck_id: '')
+    end
+  end
 end

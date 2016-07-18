f1dc61c7aff86ec557e278c053ddd84fd531b441
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-18 09:04:44 +0300

    modify user.review_card

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
 class HomeController < ApplicationController
   def index
     if current_user
-      if current_user.decks.exists?
-        @card = current_user.review_card
-      end
+      @card = current_user.review_card
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   
   has_many :decks, dependent: :destroy
   belongs_to :default_deck, class_name: 'Deck', foreign_key: "default_deck_id"
-  
+
   validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
@@ -11,9 +11,9 @@ class User < ApplicationRecord
   validates_uniqueness_of :email, case_sensitive: false
 
   def review_card
-    if default_deck_id
+    if decks.exists? && default_deck_id
       default_deck.cards.review.first
-    else
+    elsif decks.exists?
       decks.order("RANDOM()").first.cards.review.first
     end
   end

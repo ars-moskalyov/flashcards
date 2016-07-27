8cea0818b682f3ba1f37dd4b9cecb7a9b83c3f78
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-28 01:59:47 +0300

    add folders to locale

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module Flashcards
     config.action_mailer.delivery_method = :postmark
     config.action_mailer.postmark_settings = { :api_token => ENV["POSTMARK_TOKEN"] }
     config.action_mailer.default_url_options = { host: 'flashcards-62446.herokuapp.com' }
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

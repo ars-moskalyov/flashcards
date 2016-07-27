897f881da89b816471d51e7b4d7186ca504f00cf
Arseniy Moskalyov <ars.moskalyov@gmail.com>
2016-07-27 22:11:58 +0300

    fix NotificationsMailer, add spec

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -2,11 +2,14 @@ class NotificationsMailer < ActionMailer::Base
   default from: 'www@flashcards.com.de'
 
   def pending_cards_notification
-    users = User.joins(:cards).where('cards.review_date <= ?', Time.current).pluck(:email, :name, :locale).uniq!
-    users.each do |user|
-      @name = user[1]
-      I18n.with_locale(user[2]) do
-        mail(to: user[0], subject: t('mailers.notifications.subject'))
+    users = User.joins(:cards).where('cards.review_date <= ?', Time.current).pluck(:email, :name, :locale)
+    if users != nil
+      users.uniq!
+      users.each do |user|
+        @name = user[1]
+        I18n.with_locale(user[2]) do
+          mail(to: user[0], subject: t('mailers.notifications.subject'))
+        end
       end
     end
   end
